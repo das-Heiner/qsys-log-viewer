d47d8f4e29dfaadbc7dc87b97d3032758e17a7b6
Michael G <gmichael225@gmail.com>
2017-05-03 21:48:22 +0800

    Initial commit - SIP trace

---
+++ log-browser.js
@@ -0,0 +1,63 @@
+angular.module('logbrowse', [])
+
+  .run(function() {
+    console.log('browsing');
+  })
+
+  .directive("fileread", [function () {
+    return {
+      scope: {
+        fileread: "=",
+
+      },
+      link: function (scope, element, attributes) {
+        element.bind("change", function (changeEvent) {
+          var reader = new FileReader();
+          reader.onload = function (loadEvent) {
+            scope.$apply(function () {
+              scope.fileread(loadEvent.target.result);
+            });
+          }
+          reader.readAsArrayBuffer(changeEvent.target.files[0]);
+        });
+      }
+    }
+  }])
+
+  .controller('FileController', function($scope) {
+
+    $scope.files = {};
+    $scope.page = 'upload';
+
+    $scope.go = function(page) {
+      $scope.page = page;
+    }
+
+    $scope.parse = function(gzip) {
+
+      var tar = pako.inflate(gzip);
+
+      untar(tar.buffer)
+
+        .then(function(files) {
+
+          angular.forEach(files, function(file) {
+            $scope.files[file.name] = file;
+          });
+
+          var sip_trace = $scope.files['var/lib/freeswitch/log/freeswitch.log'];
+
+          $scope.$apply(function() {
+            $scope.tracePresent = !!sip_trace;
+            if(sip_trace) {
+              $scope.siplog = String.fromCharCode.apply(null, new Uint8Array(sip_trace.buffer));
+            }
+          });
+
+        }, function(err) {
+          console.error(err);
+        })
+
+    }
+
+  });
